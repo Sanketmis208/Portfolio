c940227be49a2b52ce62af5920e978b0eed69af4
Sanket Mistry <sanmist2108@gmail.com>
2026-07-14 20:56:45 +0530

    Deleted Unnecessary files

--- crop.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-img = Image.open('public/Portfolio_website_logo.png')
-# crop(left, upper, right, lower)
-cropped = img.crop((450, 480, 2450, 850))
-cropped.save('public/logo_cropped.png')
-print("Cropped saved.")
